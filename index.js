6f44ef7e3bd7237ccb92c3932cdf051ed87452f0
Henok Mekonnen <henatan99@yahoo.com>
2021-10-15 17:56:26 +0300

    Add the listFunction function in index.js

--- index.js
+++ index.js
@@ -30,3 +30,29 @@ const InfoFunction = () => {
     `
     console.log(UsageText);
 }
+
+const listFunction = () => {
+    // create a empty array
+    let data = [];
+
+    // Read from todo.txt and convert it into a string
+    const fileData = fs.readFileSync(
+        currentWorkingDirectory + 'todo.txt'
+    ).toString();
+
+    // Split the string and store into array
+    data = fileData.split('\n');
+
+    // Filter the string for any empty lines in the file
+    let filterData = data.filter(function (value) {
+        return value !== '';
+    });
+
+    if (filterData.length === 0) {
+        console.log('There are no pending todos!');
+    }
+
+    for (let i = 0; i < filterData.length; i++) {
+        console.log((filterData.length - i) + '. ' + filterData[i]);
+    }
+}
