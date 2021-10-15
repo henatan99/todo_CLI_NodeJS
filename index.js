08fb3fccf85ae683c2463a35e8e43ce6eea331fb
Henok Mekonnen <henatan99@yahoo.com>
2021-10-15 18:40:04 +0300

    Add done todo function in index.js

--- index.js
+++ index.js
@@ -146,3 +146,76 @@ const deleteFunction = () => {
         )
     }
 }
+
+const doneFunction = () => {
+    // Store the index passed as argument
+    const doneIndex = args[3];
+
+    // If argument is passed
+    if (doneIndex) {
+        // Empty array
+        let data = [];
+
+        //Create new date object 
+        let dateObj = new Date();
+
+        // Convert it to string and slice only the
+        // date part, removing the time part
+
+        let dateString = dateObj.toISOString().substring(0, 10);
+
+        // Read the data from todo.txt
+        const fileData = fs.readFileSync(
+            currentWorkingDirectory + 'done.txt'
+        ).toString();
+
+        // Split the todo.txt data 
+        data = fileData.split('\n');
+
+        // Filter for any empty lines 
+        let filterData = data.filter(function (value) {
+            return value !== '';
+        });
+
+        // If done index is greater than no. of task or <= 0
+        if (doneIndex > filterData.length || doneIndex <= 0) {
+            console.log(
+                'Error: todo #' + doneIndex + ' does not exist.'
+            );
+        } else {
+            // Delete the task from todo.txt data and storeit
+            const deleted = filterData.splice(
+                filterData.length - doneIndex, 1
+            );
+
+            // Join the array to create a string 
+            const newData = filterData.join('\n');
+
+            // Write back the data in todo.txt
+            fs.writeFile(
+                currentWorkingDirectory + 'todo.txt',
+                newData,
+                function (err) {
+                    if (err) throw err;
+                },
+            );
+
+            // Write the stored task in done.txt
+            // along with date string
+            fs.writeFile(
+                currentWorkingDirectory + 'done.txt',
+                'x ' + dateString + ' ' + deleted + '\n' + doneData,
+                function (err) {
+                    if (err) throw err;
+                    console.log('Marked todo #' + doneIndex + ' as done.');
+                },
+            );
+        }
+    } else {
+        // If argument was not passed
+        console.log(
+            'Error: Missing NUMBER for' + 
+            ' marketing todo as done.'
+        );
+    }
+};
