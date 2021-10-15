05f5e41b903e10364bb1f64d5200006521d944ac
Henok Mekonnen <henatan99@yahoo.com>
2021-10-15 18:21:04 +0300

    Add the delete todo function in index.js

--- index.js
+++ index.js
@@ -91,3 +91,58 @@ const addFunction = () => {
         console.log('Error: Missing todo string. Nothinf added!');
     }
 }
+
+const deleteFunction = () => {
+    // Store which index is passed
+    const deleteIndex = args[3];
+     
+    // If index is passed
+    if (deleteIndex) {
+        // Create a empty array 
+        let data = [];
+
+        // Read the data from file into string
+        
+        const fileData = fs.readFileSync(
+            currentWorkingDirectory + 'todo.txt'
+        ).toString();
+
+        data = fileData.split('\n');
+
+        // Filter the data from file and convert it to string
+
+        let filterData = data.filter(function (value) {
+            return value !== '';
+        });
+
+        // If delete index is greater tahn no. of task or less than zero
+        if (deleteIndex > fileData.length || deleteIndex <= 0) {
+            console.log(
+                'Error: todo #' + deleteIndex + ' does not exist. Nothing deleted.',
+            );
+        } else {
+            // Remove the task
+            filterData.splice(filterData.length - deleteIndex, 1);
+
+            // Join the array to form a string 
+            const newData = filterData.join('\n');
+
+            // Write the new data back in file
+            fs.writeFile(
+                currentWorkingDirectory + 'todo.txt',
+                newData,
+                function (err) {
+                    if (err) throw err;
+
+                    // Logs the deleted index
+                    console.log('Deleted todo #' + deleteIndex);
+                }
+            );
+        }
+    } else {
+        // Index argument was no passed
+        console.log (
+            'Error: Missing NUMBER for deleting todo.',
+        )
+    }
+}
