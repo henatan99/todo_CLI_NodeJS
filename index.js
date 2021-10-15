3d99a7468e774448331180204ecf65300c1145ec
Henok Mekonnen <henatan99@yahoo.com>
2021-10-15 17:48:07 +0300

    Add InfoText function in index.js

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ if (fs.existsSync(currentWorkingDirectory + 'done.txt') === false) {
     let createStream = fs.createWriteStream('done.txt');
     createStream.end();
 }
+
+const InfoFunction = () => {
+    const UsageText = `
+    Usage :- 
+    $ node index.js add "todo item"
+    $ node index.js ls
+    $ node index.js del NUMBER  
+    $ node index.js done NUMBER 
+    $ node index.js help
+    $ node index.js report
+    `
+    console.log(UsageText);
+}
