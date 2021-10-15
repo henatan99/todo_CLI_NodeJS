9466d2d591e194a552fbc1f45f04c338d8899cea
Henok Mekonnen <henatan99@yahoo.com>
2021-10-15 17:43:24 +0300

    Add conditions to create todo.txt and done.txt files in the current directory

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ const args = process.argv;
 //so -8 removes last 8 characters
 const currentWorkingDirectory = args[1].slice(0, -8);
 
+if (fs.existsSync(currentWorkingDirectory + 'todo.txt') === false) {
+    let createStream = fs.createWriteStream('todo.txt');
+    createStream.end();
+}
 
+if (fs.existsSync(currentWorkingDirectory + 'done.txt') === false) {
+    let createStream = fs.createWriteStream('done.txt');
+    createStream.end();
+}
